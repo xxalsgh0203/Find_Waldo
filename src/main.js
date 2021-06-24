00896739c8637aec78b02728dece3f1f444ddc16
Minho Kim <music4537@naver.com>
2021-06-24 18:32:54 +0900

    find waldo game project start

---
+++ src/main.js
@@ -0,0 +1,16 @@
+'use strict'
+
+const gamefield = document.querySelector('.game');
+const mainMenu = document.querySelector('.main_menu');
+const mainMenuBtn = document.querySelector('.main_menu_button');
+// const fieldRect = gamefield.getBoundingClientRect();
+
+
+mainMenuBtn.addEventListener('click', () => {
+    mainMenu.style.visibility = 'hidden';
+});
+
+
+function throwWaldo(){
+
+}
